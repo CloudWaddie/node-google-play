d18e0663ba1ff1c09511c386649d69c8df497358
CloudWaddie <148834837+CloudWaddie@users.noreply.github.com>
2025-05-07 08:53:05 +0930

    Delete examples directory

--- examples/reviews.js
+++
@@ -1,9 +0,0 @@
-var api = require('./common-api-init');
-
-var argv = require('minimist')(process.argv.slice(2));
-
-var pkg = argv._[0] || argv.p || 'com.viber.voip';
-
-api.reviews(pkg).then(function (reviews) {
-  console.log('%j', reviews);
-});
